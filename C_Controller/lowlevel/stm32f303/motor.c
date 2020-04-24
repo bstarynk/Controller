25f1e7d6fb6cf0c9f32a998cef3c1815f02b6c5b
Siméon Marijon <simeon.marijon@minmaxmedical.com>
2020-04-24 18:54:28 +0200

    Debug compensation

--- C_Controller/lowlevel/stm32f303/motor.c
+++ C_Controller/lowlevel/stm32f303/motor.c
@@ -207,6 +207,7 @@ uint32_t compute_motor_steps_and_Tinsu_ms(float desired_flow_Lps, float vol_mL);
 
 void test_motor() 
 {		
+	sensors_start()
 	sensors_start_sampling_flow();
 	//motor_press_constant(MOTOR_STEP_TIME_US_MIN, 3000);
 	wait(3000);
